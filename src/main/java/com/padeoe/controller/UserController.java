16d736fc6e558992202f62fb3b2d99c4a2a15212
Zafara <zhangdi1995622@qq.com>
2016-11-14 17:38:53 +0800

    增加大量内容

--- src/main/java/com/padeoe/controller/UserController.java
+++ src/main/java/com/padeoe/controller/UserController.java
@@ -2,6 +2,7 @@ package com.padeoe.controller;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import com.padeoe.pojo.User;
 import com.padeoe.service.IUserService;
@@ -16,9 +17,37 @@ import com.padeoe.pojo.User;
 public class UserController {
 	@Resource
 	private IUserService userService;
-	
+
 	@RequestMapping("/showUser")
 	public String toIndex(HttpServletRequest request,Model model){
+		int userId = Integer.parseInt(request.getParameter("id"));
+		User user = this.userService.getUserById(userId);
+		model.addAttribute("user", user);
+		return "showUser";
+	}
+
+	@RequestMapping("/login")
+	public String login(HttpServletRequest request, Model model, HttpSession session){
+		User login_user = null;
+		String return_value = "login";
+		login_user = userService.getUserByName(request.getParameter("username"));
+		if(login_user != null){
+			if(login_user.getPassword().equals(request.getParameter("password"))){
+				session.setAttribute("Id",login_user.getId());
+				return_value = "index";
+			}
+			else{
+				model.addAttribute("error", "密码输入错误");
+			}
+		}
+		else{
+			model.addAttribute("error", "未找到对应用户名");
+		}
+		return return_value;
+	}
+
+	@RequestMapping("/register")
+	public String register(HttpServletRequest request,Model model){
 		int userId = Integer.parseInt(request.getParameter("id"));
 		User user = this.userService.getUserById(userId);
 		model.addAttribute("user", user);
