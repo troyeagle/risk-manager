245af9ceec0a67ad9bbf003967e928752d900f70
刁子恒 <troy_diao@ehsy.com>
2016-11-17 16:31:54 +0800

    实现risk-condition

---
+++ src/main/java/com/padeoe/service/impl/RiskOperationServiceImpl.java
@@ -0,0 +1,55 @@
+package com.padeoe.service.impl;
+
+import com.padeoe.dao.IRiskOperationDao;
+import com.padeoe.pojo.Risk;
+import com.padeoe.pojo.RiskOperation;
+import com.padeoe.service.IRiskOperationService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * com.padeoe.service.impl
+ *
+ * @author troy_diao on 2016/11/17
+ */
+@Service("riskOperationService")
+public class RiskOperationServiceImpl implements IRiskOperationService{
+    @Resource
+    private IRiskOperationDao riskOperationDao;
+
+    @Override
+    public int saveRiskOperation(RiskOperation riskOperation) {
+        riskOperationDao.insertSelective(riskOperation);
+        return riskOperation.getId();
+    }
+
+    @Override
+    public List<RiskOperation> queryByProjectLatest(String projectName) {
+        RiskOperation query = new RiskOperation();
+        query.setProjectName(projectName);
+        return riskOperationDao.selectByConditionLatest(query);
+    }
+
+    @Override
+    public List<Risk> queryByState(int state) {
+        return riskOperationDao.queryByState(state);
+
+    }
+
+    @Override
+    public List<Risk> queryByStateLatest(int state) {
+        return riskOperationDao.queryByStateLatest(state);
+    }
+
+    @Override
+    public List<RiskOperation> queryByConditionLatest(RiskOperation condition) {
+        return riskOperationDao.selectByConditionLatest(condition);
+    }
+
+    @Override
+    public List<RiskOperation> queryByCondition(RiskOperation condition) {
+        return riskOperationDao.selectByCondition(condition);
+    }
+}
