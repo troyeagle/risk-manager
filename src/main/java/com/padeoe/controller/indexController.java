5c7e4b7c5710ca7940126c4e5fd93035e44662db
padeoe <padeoe@gmail.com>
2016-11-19 22:16:51 +0800

    查询识别和转化成问题最多的风险

--- src/main/java/com/padeoe/controller/indexController.java
+++ src/main/java/com/padeoe/controller/indexController.java
@@ -1,8 +1,10 @@
 package com.padeoe.controller;
 
 import com.padeoe.pojo.Notification;
+import com.padeoe.pojo.RiskSearchResult;
 import com.padeoe.pojo.User;
 import com.padeoe.service.INotificationService;
+import com.padeoe.service.ISearchService;
 import com.padeoe.service.IUserService;
 import com.padeoe.service.impl.NotificationServiceImpl;
 import org.springframework.stereotype.Controller;
@@ -10,9 +12,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import javax.naming.directory.SearchResult;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -22,6 +26,8 @@ import java.util.List;
 public class indexController {
     @Resource
     private INotificationService NotificationService;
+    @Resource
+    private ISearchService iSearchService;
 
     @RequestMapping("/")
     public String homepage(HttpServletRequest request, Model model) {
@@ -31,23 +37,22 @@ public class indexController {
 
     @RequestMapping("/index_page")
     public String index_page(HttpServletRequest request, Model model, HttpSession session) {
-        String username =  ((User)session.getAttribute("user")).getUsername().toString();
-        List<Notification> notifications=NotificationService.listNotification(username);
-        if(notifications==null){
-            notifications= new ArrayList<Notification>();
+        String username = ((User) session.getAttribute("user")).getUsername().toString();
+        List<Notification> notifications = NotificationService.listNotification(username);
+        if (notifications == null) {
+            notifications = new ArrayList<Notification>();
             notifications.add(new Notification());
-            model.addAttribute("notify",new ArrayList<Notification>());
-        }
-        else{
-            model.addAttribute("notify",notifications);
+            model.addAttribute("notify", new ArrayList<Notification>());
+        } else {
+            model.addAttribute("notify", notifications);
         }
         return "index";
     }
 
     @RequestMapping("/notify_page")
     public String notify_page(HttpServletRequest request, Model model) {
-        String id =  request.getParameter("id");
-        model.addAttribute("notify",NotificationService.openNotification(Integer.getInteger(id)));
+        String id = request.getParameter("id");
+        model.addAttribute("notify", NotificationService.openNotification(Integer.getInteger(id)));
         return "notify";
     }
 
@@ -66,13 +71,39 @@ public class indexController {
         return "project";
     }*/
 
-    @RequestMapping("/piecharttest_page")
-    public String piecharttest_page(HttpServletRequest request, Model model) {
-        return "piecharttest";
-    }
-
     @RequestMapping("/piechart_page")
     public String piechart_page(HttpServletRequest request, Model model) {
+        model.addAttribute("rsr",new ArrayList<>());
+        return "piechart";
+    }
+
+    @RequestMapping("/piechartsearch")
+    public String piechartShow(HttpServletRequest request, Model model) {
+        String startDate = request.getParameter("startdate");
+        String enddate = request.getParameter("enddate");
+        String type = request.getParameter("type");
+        if (type == null || startDate == null || enddate == null) {
+            model.addAttribute("error", "参数错误");
+            return "piechart_page";
+        }
+        int type_int;
+        if (type.equals("shibie")) {
+            type_int = 1;
+        }
+        else
+        if (type.equals("wenti")) {
+            type_int = 2;
+        }
+        else{
+            model.addAttribute("error", "未知的图表类型");
+            return "piechart_page";
+        }
+        System.out.println(startDate);
+        System.out.println(enddate);
+        Date start=new Date(Integer.parseInt(startDate.substring(0,4))-1900,Integer.parseInt(startDate.substring(5,7))-1,Integer.parseInt(startDate.substring(8,10)));
+        Date end=new Date(Integer.parseInt(enddate.substring(0,4))-1900,Integer.parseInt(enddate.substring(5,7))-1,Integer.parseInt(enddate.substring(8,10)));
+         List<RiskSearchResult> results=iSearchService.searchByCondition(type_int,null,start,end);
+        model.addAttribute("rsr",results);
         return "piechart";
     }
 }
