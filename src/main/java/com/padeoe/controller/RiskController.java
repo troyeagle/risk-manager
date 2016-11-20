3a457bdff392a6b3d9e36195d0d5bf681dd417d7
padeoe <padeoe@gmail.com>
2016-11-20 16:42:35 +0800

    禁止删除被调用过的风险模板

--- src/main/java/com/padeoe/controller/RiskController.java
+++ src/main/java/com/padeoe/controller/RiskController.java
@@ -1,17 +1,22 @@
 package com.padeoe.controller;
 
 import com.padeoe.pojo.Risk;
+import com.padeoe.pojo.RiskSearchResult;
 import com.padeoe.pojo.User;
 import com.padeoe.service.IRiskService;
+import com.padeoe.service.ISearchService;
 import com.padeoe.service.IUserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import javax.naming.directory.SearchResult;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by zafara on 2016/11/13.
@@ -102,25 +107,23 @@ public class RiskController {
 
     @RequestMapping("/modifyrisk_page")
     public String showChangingRisk(HttpServletRequest request, Model model) {
-        String id=request.getParameter("id");
-        if(id!=null){
-            System.out.println("风险id "+Integer.parseInt(id));
-            Risk result=iRiskService.getRiskById(Integer.parseInt(id));
-            if(result!=null){
-                    model.addAttribute("risk",result);
-                    System.out.println("查询到的风险 "+result.toString());
-                    return "modifyrisk";
+        String id = request.getParameter("id");
+        if (id != null) {
+            System.out.println("风险id " + Integer.parseInt(id));
+            Risk result = iRiskService.getRiskById(Integer.parseInt(id));
+            if (result != null) {
+                model.addAttribute("risk", result);
+                System.out.println("查询到的风险 " + result.toString());
+                return "modifyrisk";
 
-            }
-            else {
+            } else {
                 System.out.println("风险id数据库未找到");
-                model.addAttribute("error","风险id未找到");
+                model.addAttribute("error", "风险id未找到");
                 return "riskmanage";
             }
 
-        }
-        else{
-            model.addAttribute("error","你要修改的风险id不存在");
+        } else {
+            model.addAttribute("error", "你要修改的风险id不存在");
             return "riskmanage";
         }
     }
@@ -128,14 +131,20 @@ public class RiskController {
 
     @RequestMapping("/deleterisk_page")
     public String deleteRisk(HttpServletRequest request, Model model) {
-        String id=request.getParameter("id");
-        if(id==null){
-            model.addAttribute("error","没有传入要删除的风险id");
+        String id = request.getParameter("id");
+        if (id == null) {
+            model.addAttribute("error", "没有传入要删除的风险id");
+            return "redirect:riskmanage_page";
+        }
+        Set<Integer> results = iRiskService.getNonremovableRisks();
+        if (results.contains(Integer.parseInt(id))) {
+            model.addAttribute("error", "风险已经被使用，无法删除");
             return "redirect:riskmanage_page";
         }
         iRiskService.deleteRiskById(Integer.parseInt(id));
         return "redirect:riskmanage_page";
     }
+
     @RequestMapping("/modifyRiskBL")
     public String changeRisk(HttpServletRequest request, Model model) {
         Risk risk = new Risk();
@@ -153,51 +162,51 @@ public class RiskController {
         String possibility = request.getParameter("possibility");
         String influence = request.getParameter("influence");
         String threshold = request.getParameter("threshold");
-        if(id==null){
-          //  model.addAttribute("list", iRiskService.searchRisk(new Risk()));
+        if (id == null) {
+            //  model.addAttribute("list", iRiskService.searchRisk(new Risk()));
             return "redirect:riskmanage_page";
         }
         risk.setId(Integer.parseInt(id));
         //验证要修改的id是否存在，如果不存在就不去修改
-        Risk result=iRiskService.getRiskById(Integer.parseInt(id));
-        if(result==null){
-            model.addAttribute("error","要修改的项目不存在");
+        Risk result = iRiskService.getRiskById(Integer.parseInt(id));
+        if (result == null) {
+            model.addAttribute("error", "要修改的项目不存在");
             return "redirect:riskmanage_page";
         }
         if (riskBrief == null) {
-           // model.addAttribute("error", "风险简介未填写");
-            model.addAttribute("risk",result);
+            // model.addAttribute("error", "风险简介未填写");
+            model.addAttribute("risk", result);
             return "modifyrisk";
 
         }
         if (riskDetail == null) {
             model.addAttribute("error", "风险详情未填写");
-            model.addAttribute("risk",result);
+            model.addAttribute("risk", result);
             return "modifyrisk";
         }
         if (possibility == null) {
             model.addAttribute("error", "风险可能性未填写");
-            model.addAttribute("risk",result);
+            model.addAttribute("risk", result);
             return "modifyrisk";
         }
         if (influence == null) {
             model.addAttribute("error", "影响等级未填写");
-            model.addAttribute("risk",result);
+            model.addAttribute("risk", result);
             return "modifyrisk";
         }
         if (threshold == null) {
             model.addAttribute("error", "触发阈值未填写");
-            model.addAttribute("risk",result);
+            model.addAttribute("risk", result);
             return "modifyrisk";
         }
         if (!(influence.equals("致命") || influence.equals("较大") || influence.equals("一般"))) {
             model.addAttribute("error", "影响等级填写错误");
-            model.addAttribute("risk",result);
+            model.addAttribute("risk", result);
             return "modifyrisk";
         }
         if (!(possibility.equals("高") || possibility.equals("普通") || possibility.equals("低"))) {
             model.addAttribute("error", "可能性填写错误");
-            model.addAttribute("risk",result);
+            model.addAttribute("risk", result);
             return "modifyrisk";
         }
         risk.setPossibility(Risk.getLevel_Possiblility(possibility));
@@ -205,7 +214,7 @@ public class RiskController {
         risk.setRiskBrief(riskBrief);
         risk.setThreshold(threshold);
         risk.setInfluence(Risk.getLevel_Influence(influence));
-        System.out.println("最新的risk "+risk);
+        System.out.println("最新的risk " + risk);
         iRiskService.updateRisk(risk);
         return "redirect:riskmanage_page";
 
